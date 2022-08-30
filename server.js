70cfa153189c95e79e54445410ad93a09c94b6d3
Shawn102~ <shawnniamul58@gmail.com>
2022-08-30 13:40:51 +0800

    some changes in server.js file

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use("/users", usersRouter);
 
 
 // if (process.env.NODE_ENV === "production") { 
-  app.use("/", express.static("client/build")); 
+  app.use("/", express.static("todos/build")); 
     app.get("*", (req, res) => { 
   res.sendFile(path.resolve(__dirname, "todos/build/index.html")); 
   }); 
